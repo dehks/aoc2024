f0099f0c055628cd3a5943c6a2f2f88cbc49d036
Dominic Seymour <dominic.seymour@gmail.com>
2024-12-01 23:09:49 +0000

    Basic project setup with cargo-aoc

---
+++ src/day1.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn parse(_input: &str) -> i32 {
+    13
+}
+
+#[aoc(day1, part1)]
+pub fn part1(_num: &i32) -> i32 {
+    64
+}
